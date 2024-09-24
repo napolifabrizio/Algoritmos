fc31732e1df247898d0d551d08ed20520c44abc0
Fabrizio Napoli <fabrizionapoli50@gmail.com>
2024-09-24 20:51:19 -0300

    add: terminando capitulo 2 e começando o 3

--- 2 - Ordenação por seleção/exemplos.py
+++ 2 - Ordenação por seleção/exemplos.py
@@ -1,8 +1,10 @@
-
+import time
+from decimal import Decimal
+import array as arr
 
 ### Ordenação por seleção
 
-## Buscar o menor item de um array
+## Buscar o menor item de uma lista
 def buscaMenor(array):
     menor = array[0]
     indice_menor = 0
@@ -16,12 +18,96 @@ def buscaMenor(array):
 
 numeros = [10, 25, 300, 51, 6000, 8, 9, 170]
 
-## Ordena o array
-def ordenacaoPorSelecao(array):
-    novoArray = []
-    for i in range(len(array)):
-        menor = buscaMenor(array)
-        novoArray.append(array.pop(menor))
-    print(novoArray)
+## Ordena a lista
+def ordenacaoPorSelecao(lista):
+    nova_lista = []
+    for i in range(len(lista)):
+        menor = buscaMenor(lista)
+        nova_lista.append(lista.pop(menor))
+    print(nova_lista)
 
 print(ordenacaoPorSelecao(numeros))
+
+### Comparações Array x Lista
+
+def append_item_list():
+    data_list = []
+    t0 = time.time()
+    for x in range(1, 1000000):
+        data_list.append(x)
+    # print(data_list)
+    t1 = time.time()
+    res = Decimal(t1 - t0)
+    return res
+
+def append_item_array():
+    data_array = arr.array("i", [])
+    t0 = time.time()
+    for x in range(1, 1000000):
+        data_array.append(x)
+    # print(data_array)
+    t1 = time.time()
+    res = Decimal(t1 - t0)
+    return res
+
+def search_item_list():
+    data_list = [x for x in range(1, 10000000)]
+    t0 = time.time()
+    item = data_list[700000]
+    print(item)
+    t1 = time.time()
+    res = Decimal(t1 - t0)
+    return res
+
+def search_item_array():
+    data = [x for x in range(1, 10000000)]
+    data_array = arr.array("i", data)
+    t0 = time.time()
+    item = data_array[700000]
+    print(item)
+    t1 = time.time()
+    res = Decimal(t1 - t0)
+    return res
+
+def delete_item_array():
+    data = [x for x in range(1, 10000000)]
+    data_array = arr.array("i", data)
+    t0 = time.time()
+    data_array.pop(700000)
+    t1 = time.time()
+    res = Decimal(t1 - t0)
+    return res
+
+def delete_item_list():
+    data_list = [x for x in range(1, 10000000)]
+    t0 = time.time()
+    data_list.pop(700000)
+    t1 = time.time()
+    res = Decimal(t1 - t0)
+    return res
+
+# Comparações
+
+def compare_append():
+    time_array = append_item_array()
+    time_list = append_item_list()
+
+    winner = "Array" if time_array < time_list else "List"
+    print(winner)
+
+def compare_search(): # sendo analisado ainda
+    time_array = search_item_array()
+    time_list = search_item_list()
+
+    winner = "Array" if time_array < time_list else "List"
+    print(winner)
+
+def compare_delete():
+    time_array = delete_item_array()
+    time_list = delete_item_list()
+
+    winner = "Array" if time_array < time_list else "List"
+    print(winner)
+
+compare_search()
+
---
+++ 3 - Recursão/resumo.md
@@ -0,0 +1,6 @@
+
+# Recursão
+
+    Recursão é quando uma função chama ela mesma em sua lógica. Recursão não é sempre mais eficiente que loops, porém são mais claras e objetivas.
+
+Toda função recursiva tem um <b>caso-base</b> e um <b>caso-recursivo</b>. O caso-base diz quando a recursão vai parar e o caso resursivo diz quando a recursão acontece.
