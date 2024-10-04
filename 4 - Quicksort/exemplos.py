7e0ff4b9b1c6e8603b0f1c719117206405e26cb7
Fabrizio Napoli <fabrizionapoli50@gmail.com>
2024-10-03 21:39:23 -0300

    feat: terminando os exemplos do capitulo 4

--- 4 - Quicksort/exemplos.py
+++ 4 - Quicksort/exemplos.py
@@ -1,22 +1,38 @@
 
-nums = [1, 2, 3, 4, 5, 6]
+nums = [100, 22, 38, 400, 59, 61, 700, 8]
 
-def soma(lista):
-    total = 0
-    for el in lista:
-        total += el
-    return total
-
-total = soma(nums)
-
-def soma_2(lista):
+def soma(lista: list) -> int:
     total = 0
     if lista:
         el = lista.pop(len(lista)-1)
-        total = el + soma_2(lista)
+        total = el + soma(lista)
     return total
 
-total_2 = soma_2(nums)
-
+total = soma(nums)
 print(total)
-print(total_2)
+
+nums_2 = [100, 22, 38, 400, 59, 61, 700, 8]
+
+def contar(lista: list) -> int:
+    total = 0
+    if lista:
+        lista.pop(len(lista)-1)
+        total = 1 + contar(lista)
+    return total
+
+qtde = contar(nums_2)
+print(qtde)
+
+nums_3 = [100, 22, 38, 400, 59, 61, 700, 8]
+
+def quicksort(lista: list) -> list:
+    if len(lista) < 2:
+        return lista
+    pivo = lista[0]
+    menores = [i for i in lista[1:] if i <= pivo]
+    maiores = [i for i in lista[1:] if i > pivo]
+    lista_ordenada = quicksort(menores) + [pivo] + quicksort(maiores)
+    return lista_ordenada
+
+lista_ordenada = quicksort(nums_3)
+print(lista_ordenada)
